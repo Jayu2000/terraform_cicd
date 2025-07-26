e3b87e0c614d3ea330126f7b4067bc964b121663
Jayashree Patil <151853678+Jayu2000@users.noreply.github.com>
2025-07-26 23:59:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,12 @@ resource "aws_instance" "dev" {
       Name = "jayu"
     }
 }
+
+
+resource "aws_instance" "prod" {
+    ami = "ami-0f88e80871fd81e91"
+    instance_type = "t2.nano"
+    tags = {
+      Name = "jayu"
+    }
+}
